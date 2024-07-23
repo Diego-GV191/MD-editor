41b423520327ab3f1c089aec27814a030e089a31
Diego <forlanvega@gmail.com>
2024-07-23 17:47:32 -0600

    first commit

---
+++ src/renderer/src/main.tsx
@@ -0,0 +1,20 @@
+import './main.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { AppProvider, ConfigProvider, FormProvider, MDProvider } from './components'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <MDProvider>
+      <ConfigProvider>
+        <AppProvider>
+          <FormProvider>
+            <App />
+          </FormProvider>
+        </AppProvider>
+      </ConfigProvider>
+    </MDProvider>
+  </React.StrictMode>,
+)
